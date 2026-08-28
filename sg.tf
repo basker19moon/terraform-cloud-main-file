dfb716526119c87a4b6ab101df529ecee8722f2c
Bhaskar Darla <52462387+basker19moon@users.noreply.github.com>
2026-08-29 01:16:38 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,8 +1,8 @@
 module "sg" {
-  source      = "app.terraform.io/basker_bddevops/compute/modules"
+  source      = "app.terraform.io/basker_bddevops/sg/modules"
   version     = "1.0.0"
   vpc_name      = module.dev_vpc_1.vpc_name
-  #vpc_id        = module.dev_vpc_1.vpc_id
+  vpc_id        = module.dev_vpc_1.vpc_id
   environment   = module.dev_vpc_1.environment
-  #service_ports = ["80", "443", "445", "8080", "22", "3389"]
+  service_ports = ["80", "443", "445", "8080", "22", "3389"]
 }
